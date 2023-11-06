471ebd20c800b4cf002da7a8fd3e3097e5dbb9c6
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-06 17:05:27 +0100

    add: ou team grid

--- js/script.js
+++ js/script.js
@@ -37,16 +37,17 @@ for (let i = 0; i < teamMembers.length; i++) {
   let imageEl = document.createElement("img");
   let curMember = teamMembers[i];
   let singleMember = document.createElement("li");
-  singleMember.classList = "card p-5 col-12 col-md-4";
+  singleMember.classList = "p-0 col-12 col-md-6 col-lg-4 my-col p-3";
   imageEl.src = curMember.image;
+  imageEl.classList = "w-100 ";
   singleMember.innerHTML = `<li>
-                                <ul class="list-unstyled">
-                                    <li>${curMember.name}</li>
-                                    <li>${curMember.role}</li>
+                                <ul class="list-unstyled card  ms_col">
+                                    <li class="pt-3 px-3 fs-3 text-primary">${curMember.name}</li>
+                                    <li class="pb-5">${curMember.role}</li>
                                 </ul>
                             </li>`;
 
   let ulElement = singleMember.querySelector("ul");
-  ulElement.appendChild(imageEl);
+  ulElement.insertBefore(imageEl, ulElement.firstChild);
   members.appendChild(singleMember);
 }
