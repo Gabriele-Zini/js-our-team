c60db95545e950149b1a500a7d7fdc47ec4e3c44
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-06 18:17:27 +0100

    fix: img path

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@ const teamMembers = [
   {
     name: "Wayne Barnett",
     role: "Founder & CEO",
-    image: "../img/wayne-barnett-founder-ceo.jpg",
+    image: "./img/wayne-barnett-founder-ceo.jpg",
   },
   {
     name: "Angela Caroll",
     role: "Chief Editor",
-    image: "../img/angela-caroll-chief-editor.jpg",
+    image: "./img/angela-caroll-chief-editor.jpg",
   },
   {
     name: "Walter Gordon",
     role: "Office Manager",
-    image: "../img/walter-gordon-office-manager.jpg",
+    image: "./img/walter-gordon-office-manager.jpg",
   },
   {
     name: "Angela Lopez	Social",
     role: "Media Manager",
-    image: "../img/angela-lopez-social-media-manager.jpg",
+    image: "./img/angela-lopez-social-media-manager.jpg",
   },
   {
     name: "Scott Estrada",
     role: "Developer",
-    image: "../img/scott-estrada-developer.jpg",
+    image: "./img/scott-estrada-developer.jpg",
   },
   {
     name: "Barbara Ramos",
     role: "Graphic Designer",
-    image: "../img/barbara-ramos-graphic-designer.jpg",
+    image: "./img/barbara-ramos-graphic-designer.jpg",
   },
 ];
 
