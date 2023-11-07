1e446b7358d4dc00fd20de863e4c04f1ec6ffb83
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-07 10:01:20 +0100

    fix: html list strucutre

--- js/script.js
+++ js/script.js
@@ -40,12 +40,12 @@ for (let i = 0; i < teamMembers.length; i++) {
   singleMember.classList = "p-0 col-12 col-md-6 col-lg-4 my-col p-3";
   imageEl.src = curMember.image;
   imageEl.classList = "w-100 ";
-  singleMember.innerHTML = `<li>
+  singleMember.innerHTML = `
                                 <ul class="list-unstyled card  ms_col">
                                     <li class="pt-3 px-3 fs-3 text-primary">${curMember.name}</li>
                                     <li class="pb-5">${curMember.role}</li>
                                 </ul>
-                            </li>`;
+                            `;
 
   let ulElement = singleMember.querySelector("ul");
   ulElement.insertBefore(imageEl, ulElement.firstChild);
